70c61b3a993d61ca37979821def0ba20ba503b95
Vighnesh Souda <29220608+vsouda@users.noreply.github.com>
2018-02-24 00:54:09 -0500

    Add minimax algorithm

--- API.py
+++ API.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request, make_response
-
+from TicTacToe import *
+import random, TicTacToe
 
 app = Flask(__name__)
 
@@ -10,8 +11,17 @@ def post_tasks():
 	ht = request.get_json()
 	if ("playerSymbol" not in ht or "gameBoard" not in ht):
 		return make_response(jsonify({'error': 'Could not find playerSymbol and gameBoard'}), 400)
+	gameboard = ht["gameBoard"]
+	gameboard = [None if i=="?" else i for i in gameboard]
+	tic = Tic(gameboard)
+	tic.show()
+	a = determine(tic,ht["playerSymbol"])
+	tic.make_move(a, ht["playerSymbol"])
+	gameboard = ["?" if i==None else i for i in tic.get_gameboard()]
+	tic.show()
 	return(request.data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
 
