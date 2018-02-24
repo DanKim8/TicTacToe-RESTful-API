165e5b8c07e4dc08a9f7fd0f63236bd607299bee
Vighnesh Souda <29220608+vsouda@users.noreply.github.com>
2018-02-24 00:59:31 -0500

    Finish API, return new gameboard

--- API.py
+++ API.py
@@ -1,7 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request, make_response
 from TicTacToe import *
-import random, TicTacToe
 
 app = Flask(__name__)
 
@@ -9,17 +8,21 @@ app = Flask(__name__)
 @app.route('/api/v1/tictactoe', methods=['POST'])
 def post_tasks():
 	ht = request.get_json()
-	if ("playerSymbol" not in ht or "gameBoard" not in ht):
-		return make_response(jsonify({'error': 'Could not find playerSymbol and gameBoard'}), 400)
-	gameboard = ht["gameBoard"]
+	if ("playerSymbol" not in ht or "gameboard" not in ht):
+		return make_response(jsonify({'error': 'Could not find playerSymbol and gameboard'}), 400)
+	gameboard = ht["gameboard"]
 	gameboard = [None if i=="?" else i for i in gameboard]
 	tic = Tic(gameboard)
 	tic.show()
 	a = determine(tic,ht["playerSymbol"])
 	tic.make_move(a, ht["playerSymbol"])
 	gameboard = ["?" if i==None else i for i in tic.get_gameboard()]
+	print("------------------")
 	tic.show()
-	return(request.data)
+	return(jsonify(
+		new_move=a,
+		new_gameboard=gameboard
+	))
 
 if __name__ == '__main__':
 	app.run(debug=True)
